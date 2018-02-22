56eab776dcedcf866db20d26a6e707d3150c7e19
Enrique <enriquefromojaro@gmail.com>
2018-02-22 21:41:21 +0100

    Replace multiple calls to for loops

--- main.js
+++ main.js
@@ -11,71 +11,42 @@ print("");
 //print("SE SELECCIONA EL FICHERO MCU-ID FILE FILE FF00");
 resp = card.openMCUIDFile();
 print("          MCU-ID FILE FILE FF00");
-resp = card.readRecord(0, 0, 8);
-print("");
-print("MCU-ID FILE Record Nº 0:" + "  " + resp + "   " + resp.toString(ASCII));
-resp = card.readRecord(1, 0, 8);
-print("MCU-ID FILE Record Nº 1:" + "  " + resp + "   " + resp.toString(ASCII));
-print("");
+for (var i=0; i < 2; i++){
+	resp = card.readRecord(i, 0, 8);
+	print("");
+	print("MCU-ID FILE Record Nº 0" + i + ":" + "  " + resp + "   " + resp.toString(ASCII));
+}
+
 
 print("          MANUFACTURER FILE FF01");
 resp = card.openManufacturerFile();
 print("");
 print("MANUFACTURER FILE FF01 TIENE 2 RECORDS DE 8 BYTES.");
-resp = card.readRecord(0, 0, 8);
-print("");
-print("MANUFACTURER FILE Record Nº 00:" + "  " + resp);
-resp = card.readRecord(1, 0, 8);
-print("MANUFACTURER FILE Record Nº 01:" + "  " + resp);
+for (var i=0; i < 2; i++){
+	resp = card.readRecord(i, 0, 8);
+	print("");
+	print("MANUFACTURER FILE Record Nº 0" + i + ":" + "  " + resp + "   " + resp.toString(ASCII));
+}
 print("");
 
 //print("          PERSONALIZATION FILE FF02");
 resp = card.openPersonalizationFile();
 print("PERSONALIZATION FILE FF02 TIENE 3 RECORDS DE 4 BYTES.");
-resp = card.readRecord(0, 0, 4);
-print("");
-print("PERSONALIZATION FILE Record Nº 00:" + "  " + resp);
-resp = card.readRecord(1, 0, 4);
-print("PERSONALIZATION FILE Record Nº 01:" + "  " + resp);
-resp = card.readRecord(2, 0, 4);
-print("PERSONALIZATION FILE Record Nº 02:" + "  " + resp);
+for (var i=0; i < 3; i++){
+	resp = card.readRecord(i, 0, 4);
+	print("");
+	print("PERSONALIZATION FILE Record Nº 0" + i + ":" + "  " + resp + "   " + resp.toString(ASCII));
+}
 print("");
 
 
 print("          FICHERO SECURITY FILE FF03 TIENE 14 RECORDS DE 8 BYTES");
 resp = card.openSecurityFile();
-print("");
-
-resp = card.readRecord(0, 0, 8);
-print("SECURITY FILE Record Nº 00:" + "  " + resp + "   " + resp.toString(ASCII));
-
-resp = card.readRecord(1, 0, 8);
-print("SECURITY FILE Record Nº 01:" + "  " + resp + "   " + resp.toString(ASCII));
-
-resp = card.readRecord(2, 0, 8);
-print("SECURITY FILE Record Nº 02:" + "  " + resp);
-
-resp = card.readRecord(3, 0, 8);
-print("SECURITY FILE Record Nº 03:" + "  " + resp);
-
-resp = card.readRecord(4, 0, 8);
-print("SECURITY FILE Record Nº 04:" + "  " + resp);
-
-resp = card.readRecord(5, 0, 8);
-print("SECURITY FILE Record Nº 05:" + "  " + resp);
-
-resp = card.readRecord(6, 0, 8);
-print("SECURITY FILE Record Nº 06:" + "  " + resp);
-
-resp = card.readRecord(7, 0, 8);
-print("SECURITY FILE Record Nº 07:" + "  " + resp);
-
-resp = card.readRecord(8, 0, 8);
-print("SECURITY FILE Record Nº 08:" + "  " + resp);
-
-resp = card.readRecord(9, 0, 8);
-print("SECURITY FILE Record Nº 09:" + "  " + resp);
-
+for (var i=0; i < 10; i++){
+	resp = card.readRecord(i, 0, 4);
+	print("");
+	print("SECURITY FILE Record Nº 0" + i + ":" + "  " + resp + "   " + resp.toString(ASCII));
+}
 resp = card.readRecord(0x0A, 0, 8);
 print("SECURITY FILE Record Nº 0A:" + "  " + resp);
 
@@ -91,25 +62,12 @@ print("");
 
 print("          FICHERO USER FILE MANAGEMENT FF04");
 resp = card.openUserManagFile();
+
 print("");
-
-resp = card.readRecord(0, 0, 7);
-print("USER FILE MANAGEMENT Record Nº 00:" + "  " + resp);
-resp = card.readRecord(1, 0, 8);
-print("USER FILE MANAGEMENT Record Nº 01:" + "  " + resp);
-resp = card.readRecord(2, 0, 8);
-print("USER FILE MANAGEMENT Record Nº 02:" + "  " + resp);
-resp = card.readRecord(3, 0, 8);
-print("USER FILE MANAGEMENT Record Nº 03:" + "  " + resp);
-resp = card.readRecord(4, 0, 8);
-print("USER FILE MANAGEMENT Record Nº 04:" + "  " + resp);
-resp = card.readRecord(5, 0, 8);
-print("USER FILE MANAGEMENT Record Nº 05:" + "  " + resp);
-resp = card.readRecord(6, 0, 8);
-print("USER FILE MANAGEMENT Record Nº 06:" + "  " + resp);
-resp = card.readRecord(7, 0, 8);
-print("USER FILE MANAGEMENT Record Nº 07:" + "  " + resp);
-
+for (var i=0; i < 8; i++){
+	resp = card.readRecord(i, 0, 7);
+	print("USER FILE MANAGEMENT Record Nº 0" + i + ":" + "  " + resp + "   " + resp.toString(ASCII));
+}
 print("Código SW:" + card.SW.toString(16));
 print("");
 
@@ -118,60 +76,23 @@ print("          FICHERO ACCOUNT FILE FF05 OCHO RECORDS DE CUATRO BYTES");
 resp = card.openAccountFile();
 print("");
 
-resp = card.readRecord(0, 0, 4);
-print("ACCOUNT FILE Record Nº 00:" + "  " + resp);
-
-resp = card.readRecord(1, 0, 4);
-print("ACCOUNT FILE Record Nº 01:" + "  " + resp);
-
-resp = card.readRecord(2, 0, 4);
-print("ACCOUNT FILE Record Nº 02:" + "  " + resp);
-
-resp = card.readRecord(3, 0, 4);
-print("ACCOUNT FILE Record Nº 03:" + "  " + resp);
-
-resp = card.readRecord(4, 0, 4);
-print("ACCOUNT FILE Record Nº 04:" + "  " + resp);
-
-resp = card.readRecord(5, 0, 4);
-print("ACCOUNT FILE Record Nº 05:" + "  " + resp);
-
-resp = card.readRecord(6, 0, 4);
-print("ACCOUNT FILE Record Nº 06:" + "  " + resp);
-
-resp = card.readRecord(7, 0, 4);
-print("ACCOUNT FILE Record Nº 07:" + "  " + resp);
+for (var i=0; i < 8; i++){
+	resp = card.readRecord(i, 0, 4);
+	print("ACCOUNT FILE Record Nº 0" + i + ":" + "  " + resp + "   " + resp.toString(ASCII));
+}
 print("");
 
 print("          FICHERO ACCOUNT SECURETY FILE FF06 8 RECORDS DE OCHO BYTES");
 resp = card.openAccountSeverityFile();
 print("");
 
-resp = card.readRecord(0, 0, 8);
-print("ACCOUNT FILE Record Nº 00:" + "  " + resp);
-
-resp = card.readRecord(1, 0, 8);
-print("ACCOUNT FILE Record Nº 01:" + "  " + resp);
-
-resp = card.readRecord(2, 0, 8);
-print("ACCOUNT FILE Record Nº 02:" + "  " + resp);
-
-resp = card.readRecord(3, 0, 8);
-print("ACCOUNT FILE Record Nº 03:" + "  " + resp);
-
-resp = card.readRecord(4, 0, 8);
-print("ACCOUNT FILE Record Nº 04:" + "  " + resp);
-
-resp = card.readRecord(5, 0, 8);
-print("ACCOUNT FILE Record Nº 05:" + "  " + resp);
-
-resp = card.readRecord(6, 0, 8);
-print("ACCOUNT FILE Record Nº 06:" + "  " + resp);
-
-resp = card.readRecord(7, 0, 8);
-print("ACCOUNT FILE Record Nº 07:" + "  " + resp);
-
+for (var i=0; i < 8; i++){
+	resp = card.readRecord(i, 0, 8);
+	print("ACCOUNT FILE Record Nº 0" + i + ":" + "  " + resp + "   " + resp.toString(ASCII));
+}
 print();
+
+
 print("          FICHERO ATR FILE FF07 UN RECORDS DE 36 BYTES");
 resp = card.openATRFile();
 print("");
