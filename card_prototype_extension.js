944b20cfc3e8efc01d9912acb46a3f041a2edc55
Enrique <enriquefromojaro@gmail.com>
2018-02-22 21:16:12 +0100

    Add method readRecord to card prototype

--- card_prototype_extension.js
+++ card_prototype_extension.js
@@ -1,6 +1,18 @@
-Card.prototype.readFile = readFile;
+Card.prototype.openFile = openFile;
 Card.prototype.presentIC = presentIC;
 
+Card.prototype.readRecord = readRecord;
+Card.prototype.openMCUIDFile = openMCUIDFile;
+Card.prototype.openManufacturerFile = openManufacturerFile;
+Card.prototype.openPersonalizationFile = openPersonalizationFile;
+Card.prototype.openSecurityFile = openSecurityFile;
+Card.prototype.openUserManagFile = openUserManagFile;
+Card.prototype.openAccountFile = openAccountFile;
+Card.prototype.openAccountSeverityFile = openAccountSeverityFile;
+Card.prototype.openATRFile = openATRFile;
+
+
+// ----------------------------   SEVERAL PURPOSE METHODS
 
 function presentIC(){
 	print("PRESENTACION del IC para iniciar: ACOSTEST= 41434F53 54455354");
@@ -8,9 +20,46 @@ function presentIC(){
 	print("Código SW: " + card.SW.toString(16));
 }
 
-function readFile (file){
+function openFile(file){
 	var apduCommand = new ByteString("80 A4 00 00 02", HEX);
 	var wholeCommand = apduCommand.concat(new ByteString(file, HEX));
 	return this.plainApdu(wholeCommand);
 }
 
+function readRecord(record, offset, length){
+	return this.sendApdu(0x80, 0xB2, record, offset, length);
+}
+
+// ----------------------------  CONCRETE FILE METHODS
+
+function openMCUIDFile(){
+	return this.openFile('FF00');
+}
+
+function openManufacturerFile(){
+	return this.openFile('FF01');
+}
+
+function openPersonalizationFile(){
+	return this.openFile('FF02');
+}
+
+function openSecurityFile(){
+	return this.openFile('FF03');
+}
+
+function openUserManagFile(){
+	return this.openFile('FF04');
+}
+
+function openAccountFile(){
+	return this.openFile('FF05');
+}
+
+function openAccountSeverityFile(){
+	return this.openFile('FF06');
+}
+
+function openATRFile(){
+	return this.openFile('FF07');
+}
--- main.js
+++ main.js
@@ -9,105 +9,105 @@ card.presentIC()
 print("");
 
 //print("SE SELECCIONA EL FICHERO MCU-ID FILE FILE FF00");
-resp = card.readFile('FF00');
+resp = card.openMCUIDFile();
 print("          MCU-ID FILE FILE FF00");
-resp = card.plainApdu(new ByteString("80 B2 00 00 08", HEX));
+resp = card.readRecord(0, 0, 8);
 print("");
 print("MCU-ID FILE Record Nº 0:" + "  " + resp + "   " + resp.toString(ASCII));
-resp = card.plainApdu(new ByteString("80 B2 01 00 08", HEX));
+resp = card.readRecord(1, 0, 8);
 print("MCU-ID FILE Record Nº 1:" + "  " + resp + "   " + resp.toString(ASCII));
 print("");
 
 print("          MANUFACTURER FILE FF01");
-resp = card.readFile('FF01');
+resp = card.openManufacturerFile();
 print("");
 print("MANUFACTURER FILE FF01 TIENE 2 RECORDS DE 8 BYTES.");
-resp = card.plainApdu(new ByteString("80 B2 00 00 08", HEX));
+resp = card.readRecord(0, 0, 8);
 print("");
 print("MANUFACTURER FILE Record Nº 00:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 01 00 08", HEX));
+resp = card.readRecord(1, 0, 8);
 print("MANUFACTURER FILE Record Nº 01:" + "  " + resp);
 print("");
 
 //print("          PERSONALIZATION FILE FF02");
-resp = card.readFile('FF02');
+resp = card.openPersonalizationFile();
 print("PERSONALIZATION FILE FF02 TIENE 3 RECORDS DE 4 BYTES.");
-resp = card.plainApdu(new ByteString("80 B2 00 00 04", HEX));
+resp = card.readRecord(0, 0, 4);
 print("");
 print("PERSONALIZATION FILE Record Nº 00:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 01 00 04", HEX));
+resp = card.readRecord(1, 0, 4);
 print("PERSONALIZATION FILE Record Nº 01:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 02 00 04", HEX));
+resp = card.readRecord(2, 0, 4);
 print("PERSONALIZATION FILE Record Nº 02:" + "  " + resp);
 print("");
 
 
 print("          FICHERO SECURITY FILE FF03 TIENE 14 RECORDS DE 8 BYTES");
-resp = card.readFile('FF03');
+resp = card.openSecurityFile();
 print("");
 
-resp = card.plainApdu(new ByteString("80 B2 00 00 08", HEX));
+resp = card.readRecord(0, 0, 8);
 print("SECURITY FILE Record Nº 00:" + "  " + resp + "   " + resp.toString(ASCII));
 
-resp = card.plainApdu(new ByteString("80 B2 01 00 08", HEX));
+resp = card.readRecord(1, 0, 8);
 print("SECURITY FILE Record Nº 01:" + "  " + resp + "   " + resp.toString(ASCII));
 
-resp = card.plainApdu(new ByteString("80 B2 02 00 08", HEX));
+resp = card.readRecord(2, 0, 8);
 print("SECURITY FILE Record Nº 02:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 03 00 08", HEX));
+resp = card.readRecord(3, 0, 8);
 print("SECURITY FILE Record Nº 03:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 04 00 08", HEX));
+resp = card.readRecord(4, 0, 8);
 print("SECURITY FILE Record Nº 04:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 05 00 08", HEX));
+resp = card.readRecord(5, 0, 8);
 print("SECURITY FILE Record Nº 05:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 06 00 08", HEX));
+resp = card.readRecord(6, 0, 8);
 print("SECURITY FILE Record Nº 06:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 07 00 08", HEX));
+resp = card.readRecord(7, 0, 8);
 print("SECURITY FILE Record Nº 07:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 08 00 08", HEX));
+resp = card.readRecord(8, 0, 8);
 print("SECURITY FILE Record Nº 08:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 09 00 08", HEX));
+resp = card.readRecord(9, 0, 8);
 print("SECURITY FILE Record Nº 09:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 0A 00 08", HEX));
+resp = card.readRecord(0x0A, 0, 8);
 print("SECURITY FILE Record Nº 0A:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 0B 00 08", HEX));
+resp = card.readRecord(0x0B, 0, 8);
 print("SECURITY FILE Record Nº 0B:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 0C 00 08", HEX));
+resp = card.readRecord(0x0C, 0, 8);
 print("SECURITY FILE Record Nº 0C:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 0D 00 08", HEX));
+resp = card.readRecord(0x0D, 0, 8);
 print("SECURITY FILE Record Nº 0D:" + "  " + resp);
 print("");
 
 print("          FICHERO USER FILE MANAGEMENT FF04");
-resp = card.readFile('FF04');
+resp = card.openUserManagFile();
 print("");
 
-resp = card.plainApdu(new ByteString("80 B2 00 00 07", HEX));
+resp = card.readRecord(0, 0, 7);
 print("USER FILE MANAGEMENT Record Nº 00:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 01 00 07", HEX));
+resp = card.readRecord(1, 0, 8);
 print("USER FILE MANAGEMENT Record Nº 01:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 02 00 07", HEX));
+resp = card.readRecord(2, 0, 8);
 print("USER FILE MANAGEMENT Record Nº 02:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 03 00 07", HEX));
+resp = card.readRecord(3, 0, 8);
 print("USER FILE MANAGEMENT Record Nº 03:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 04 00 07", HEX));
+resp = card.readRecord(4, 0, 8);
 print("USER FILE MANAGEMENT Record Nº 04:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 05 00 07", HEX));
+resp = card.readRecord(5, 0, 8);
 print("USER FILE MANAGEMENT Record Nº 05:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 06 00 07", HEX));
+resp = card.readRecord(6, 0, 8);
 print("USER FILE MANAGEMENT Record Nº 06:" + "  " + resp);
-resp = card.plainApdu(new ByteString("80 B2 07 00 07", HEX));
+resp = card.readRecord(7, 0, 8);
 print("USER FILE MANAGEMENT Record Nº 07:" + "  " + resp);
 
 print("Código SW:" + card.SW.toString(16));
@@ -115,68 +115,69 @@ print("");
 
 
 print("          FICHERO ACCOUNT FILE FF05 OCHO RECORDS DE CUATRO BYTES");
-resp = card.readFile('FF05');
+resp = card.openAccountFile();
 print("");
 
-resp = card.plainApdu(new ByteString("80 B2 00 00 04", HEX));
+resp = card.readRecord(0, 0, 4);
 print("ACCOUNT FILE Record Nº 00:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 01 00 04", HEX));
+resp = card.readRecord(1, 0, 4);
 print("ACCOUNT FILE Record Nº 01:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 02 00 04", HEX));
+resp = card.readRecord(2, 0, 4);
 print("ACCOUNT FILE Record Nº 02:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 03 00 04", HEX));
+resp = card.readRecord(3, 0, 4);
 print("ACCOUNT FILE Record Nº 03:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 04 00 04", HEX));
+resp = card.readRecord(4, 0, 4);
 print("ACCOUNT FILE Record Nº 04:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 05 00 04", HEX));
+resp = card.readRecord(5, 0, 4);
 print("ACCOUNT FILE Record Nº 05:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 06 00 04", HEX));
+resp = card.readRecord(6, 0, 4);
 print("ACCOUNT FILE Record Nº 06:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 07 00 04", HEX));
+resp = card.readRecord(7, 0, 4);
 print("ACCOUNT FILE Record Nº 07:" + "  " + resp);
 print("");
 
 print("          FICHERO ACCOUNT SECURETY FILE FF06 8 RECORDS DE OCHO BYTES");
-resp = card.readFile('FF06');
+resp = card.openAccountSeverityFile();
 print("");
 
-resp = card.plainApdu(new ByteString("80 B2 00 00 08", HEX));
+resp = card.readRecord(0, 0, 8);
 print("ACCOUNT FILE Record Nº 00:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 01 00 08", HEX));
+resp = card.readRecord(1, 0, 8);
 print("ACCOUNT FILE Record Nº 01:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 02 00 08", HEX));
+resp = card.readRecord(2, 0, 8);
 print("ACCOUNT FILE Record Nº 02:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 03 00 08", HEX));
+resp = card.readRecord(3, 0, 8);
 print("ACCOUNT FILE Record Nº 03:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 04 00 08", HEX));
+resp = card.readRecord(4, 0, 8);
 print("ACCOUNT FILE Record Nº 04:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 05 00 08", HEX));
+resp = card.readRecord(5, 0, 8);
 print("ACCOUNT FILE Record Nº 05:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 06 00 08", HEX));
+resp = card.readRecord(6, 0, 8);
 print("ACCOUNT FILE Record Nº 06:" + "  " + resp);
 
-resp = card.plainApdu(new ByteString("80 B2 07 00 08", HEX));
+resp = card.readRecord(7, 0, 8);
 print("ACCOUNT FILE Record Nº 07:" + "  " + resp);
 
 print();
 print("          FICHERO ATR FILE FF07 UN RECORDS DE 36 BYTES");
-resp = card.readFile('FF07');
+resp = card.openATRFile();
 print("");
 
-resp = card.plainApdu(new ByteString("80 B2 00 00 24", HEX));
+resp = card.readRecord(0, 0, 0x24);
+
 print("ATR FILE Record Nº 00:" + "  " + resp);
 
 print("");
