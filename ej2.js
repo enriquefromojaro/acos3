c7ba3293a103f7775f4e7cb6dd68f98ea22519ac
Enrique <enriquefromojaro@gmail.com>
2018-02-24 11:01:18 +0100

    Program exercise with dangerous code commented and no test

--- ej2.js
+++ ej2.js
@@ -2,3 +2,36 @@ load('utils.js');
 load('card_prototype_extension.js');
 
 var card = new Card();
+var atr = card.reset(card.COLD_RESET);
+
+// We present the IC in order to get root access
+
+var icResult = card.presentIC()
+
+if (icResult.status !== '9000'){
+    print('[ERROR] Error on presenting IC. Closing connection');
+    card.close();
+    return null;
+}
+
+// Opening the personalization file
+
+
+var resp = card.openPersonalizationFile();
+if (resp.status !== '9000'){
+	print('[ERROR] Error on opening personalization file')
+	return null;
+}
+
+/************** NO DESCOMENTAR HASTA ESTAR SEGUROSE QUE ESTÁ BIEN
+//Writing Ooption register byte
+card.writeRecord(0, 0, 1, new ByteString('25', HEX));
+
+// Writing Security option register. I'm not sure if it should be 0x14 or 0x54
+card.writeRecord(1, 0, 1, new ByteString('54', HEX));
+
+// Writing number_pf_files register
+card.writeRecord(1, 0, 1, new ByteString('04', HEX));
+
+*/
+
