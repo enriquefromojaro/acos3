8dc08689c8dd642a32aaba86cace8c42f6ab9ecb
Enrique <enriquefromojaro@gmail.com>
2018-02-25 11:50:26 +0100

    done binary files

--- ej7.js
+++ ej7.js
@@ -2,73 +2,85 @@ load('utils.js');
 load('card_prototype_extension.js');
 
 ej7 = {
-    main: function () {
+    main : function() {
 
 	var card = new Card();
 	var atr = card.reset(Card.RESET_COLD);
-	
+
 	// No authentication is required to read and write the files
-	
-	// Opening binary file 8DC1
-	var resp = card.openFile('8DC1');
-	if (resp.status !== '9000'){
-		print('[ERROR] Error on opening file 8DC1: ' + resp.status)
-		return null;
+
+	// Binary file 8DC1
+	var fileName = '8DC1';
+	var resp = card.openFile(fileName);
+	if (resp.status !== '9000' && ! resp.status.substring(0, 2) ==='91') {
+	    print('[ERROR] Error on opening file 8DC1: ' + resp.status)
+	    return null;
 	}
-	print('BEFORE: ' + card.reaBinary(0, 0, 256).data);
-	
-	var str = this.getFileText(fileName, recordLength);
+
+	var str = this.getFileText(fileName, 256);
+	print (str)
 	resp = card.writeBinary(0, 0, str.length, new ByteString(str, ASCII));
-	print('AFTER: ' + card.reaBinary(0, 0, 256).data);
+	if (resp.status !== '9000') {
+	    print('[ERROR] Error on writing file 8DC1: ' + resp.status)
+	    return null;
+	}	
+	
+	print('-----------------------------------------------------------------------')
 	
 	
-/*	// Writing debit key
-	resp = card.writeRecord(0, 0, 8, new ByteString('80 81 82 83 84 85 86 87', HEX));
-	if (resp.status !== '9000'){
-		print('[ERROR] Error on writing debit key')
-		return null;
+	// Binary file 8DC2
+	fileName = '8DC2'
+	var resp = card.openFile(fileName);
+	if (resp.status !== '9000' && ! resp.status.substring(0, 2) ==='91') {
+	    print('[ERROR] Error on opening file 8DC1: ' + resp.status)
+	    return null;
 	}
 	
-	// Writing credit key
-	resp = card.writeRecord(1, 0, 8, new ByteString('90 91 92 93 94 95 96 97', HEX));
-	if (resp.status !== '9000'){
-		print('[ERROR] Error on writing credit key')
-		return null;
+	
+	// in apdu write command, length is defined by 1 Byte, so we cannot write more than 255 bytes
+	// in a time. In this case, is more than enough
+	var str = this.getFileText(fileName, 255);
+	print (str)
+	resp = card.writeBinary(0, 0, str.length, new ByteString(str, ASCII));
+	if (resp.status !== '9000') {
+	    print('[ERROR] Error on writing file 8DC2: ' + resp.status)
+	    return null;
 	}
 	
-	// Writing certify key
-	resp = card.writeRecord(2, 0, 8, new ByteString('A0 A1 A2 A3 A4 A5 A6 A7', HEX));
-	if (resp.status !== '9000'){
-		print('[ERROR] Error on certify key')
-		return null;
+	print('-----------------------------------------------------------------------')
+	// record file 8DC3 127 records * 16B
+	fileName = '8DC3'
+	var resp = card.openFile(fileName);
+	if (resp.status !== '9000' && ! resp.status.substring(0, 2) ==='91') {
+	    print('[ERROR] Error on opening file 8DC3: ' + resp.status)
+	    return null;
 	}
 	
-	// Writing revoke debit key
-	resp = card.writeRecord(3, 0, 8, new ByteString('B0 B1 B2 B3 B4 B5 B6 B7', HEX));
-	if (resp.status !== '9000'){
-		print('[ERROR] Error on revoke debit key')
-		return null;
-	}*/
+	print('BEFORE: ' + card.readRecord(0, 0, 16).data.toString(ASCII));
+	
+	// in apdu write command, length is defined by 1 Byte, so we cannot write more than 255 bytes
+	// in a time. In this case, is more than enough
+	var str = this.getFileText(fileName, 15);
+	print (str)
+	resp = card.writeRecord(0, 0, str.length, new ByteString(str, ASCII));
+	if (resp.status !== '9000') {
+	    print('[ERROR] Error on writing file 8DC3: ' + resp.status)
+	    return null;
+	}
+	print('AFTER: ' + card.readBinary(0, 0, 16).data.toString(ASCII));
 	
     },
-    
-    getFileText: function (fileName, recordLength){
+
+    getFileText : function(fileName, recordLength) {
 	var alumnName = 'Enrique';
 	var quijote = 'En un lugar de la mancha de cuyo nombre no quiero acordarme';
 	var subject = 'Aplicaciones para Smart Cards';
 	var acadCourse = '2017 2018'
-	
-	var result = [
-	    fileName,
-	    alumnName,
-	    'MASTER DE SISTEMAS',
-	    subject,
-	    acadCourse,
-	    fileName,
-	    quijote
-	];
-	
-	return result.join(' ').sustring(recordLength);
+
+	var result = [ fileName, alumnName, 'MASTER DE SISTEMAS', subject,
+		acadCourse, fileName, quijote ];
+
+	return result.join(' ').substring(0, recordLength);
 
     }
 
