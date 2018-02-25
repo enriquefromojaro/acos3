aac455b98b7ccbccbec0e47736d8141338286ed9
Enrique <enriquefromojaro@gmail.com>
2018-02-25 19:02:27 +0100

    Add methods to encrypt and decrypt DES with ECB

--- utils.js
+++ utils.js
@@ -1,20 +1,39 @@
 Utils = {
-		numbers: {},
-		bytes: {}
+    numbers : {},
+    bytes : {}
 };
 
-Utils.numbers.fixedLengthIntString = function (num, length){
-	return ("00000000000000000" + num).slice(-1*length);
+Utils.numbers.fixedLengthIntString = function(num, length) {
+    return ("00000000000000000" + num).slice(-1 * length);
 }
 
-Utils.bytes.calcChecksum = function (bytes){
-    var resultBS = new ByteString('00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00', HEX);
-    for(var i=0; i<bytes.length; i++){
+Utils.bytes.calcChecksum = function(bytes) {
+    var resultBS = new ByteString(
+	    '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00', HEX);
+    for (var i = 0; i < bytes.length; i++) {
 	resultBS = resultBS.add(bytes.byteAt(i))
     }
     resultBS = resultBS.add(1)
     return resultBS.right(1);
 }
 
+Utils.bytes.encryptECB = function (plain, cypherKey) {
+    var crypto = new Crypto();
+    var key = new Key();
+    key.setComponent(Key.DES, cypherKey);
+
+    var cyphered = crypto.encrypt(key, Crypto.DES_ECB, plain);
+
+    return cyphered;
+}
 
 
+Utils.bytes.decryptECB = function (crypted, cypherKey) {
+
+    var crypto = new Crypto();
+    var key = new Key();
+    key.setComponent(Key.DES, cypherKey);
+
+    var decrypted = crypto.decrypt(key, Crypto.DES_ECB, crypted);
+    return decrypted;
+}
