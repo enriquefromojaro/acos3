53d86240c58587b0d89c051933f2d80944ad195a
Enrique <enriquefromojaro@gmail.com>
2018-03-04 19:09:10 +0100

    revoke debit with SM

---
+++ ej20_revoke_debit.js
@@ -0,0 +1,61 @@
+load('utils.js');
+load('card_prototype_extension.js');
+
+ej20 = {
+    main : function() {
+
+	var card = new Card();
+	var atr = card.reset(Card.RESET_COLD);
+	
+	var authenticated = card.authenticate();
+	if (! authenticated){
+	    print('[ERROR] Not authenticated!!! Access denied!!');
+	    return null;
+	}else{
+	    print('Autehnticated!!! access garanteed!!');
+	}
+
+	resp = this.inquireAccount(card);
+	if(! resp)
+	    return null;
+	card.seqNum = card.seqNum.add(1);
+	    
+	resp = card.revokeDebit(12.50 *100, resp, true);
+	if (resp.status !== '9000' && resp.status !== '6882'){
+	    print('[ERROR] Error on credit operation: ' + resp.status);
+	    return null;
+	}
+	
+	card.seqNum = card.seqNum.add(1);
+	print('-----------------------------');
+	resp = this.inquireAccount(card);
+	if(! resp)
+	    return null;
+	
+    },
+    inquireAccount: function(card){
+	var resp = card.inquireAccount(2, new ByteString('00 00 00 00', HEX), true);
+	if(resp.status !== '9000' && resp.status !== '6882'){
+	    print('[ERROR] Error on inquire account: ' + resp.status);
+	}
+	
+	resp = card.getInquireAccountResponse(new ByteString('00 00 00 00', HEX), 2, true);
+	print(resp.status);
+	if (resp.status === '9000'){
+	    print('MAC:           ' + resp.MAC);
+	    print('Trans. Type:   ' + resp.transType);
+	    print('Balance:       ' + resp.balance);
+	    print('ATREF:         ' + resp.atref);
+	    print('Max. Balance:  ' + resp.maxBalance);
+	    print('Credit Entity: ' + resp.creditEntity);
+	    print('Debit Entity:  ' + resp.debitEntity);
+	    return resp;
+	}
+	else
+	    return null
+    }
+
+};
+
+//ej20.main();
+//readcard();
